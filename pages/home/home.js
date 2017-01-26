46764c1b7692d23c5293d985e5f0388d57484765
banshengbushu <wangdanna1995@outlook.com>
2017-01-27 02:11:26 +0800

    Add search icon

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,23 +2,23 @@ Page({
   data: {
     inputName: '',
   },
-  bindNameInput: function (e) {
+  onSearch: function (e) {
+    console.log(e)
+    if (this.data.inputName === '') {
+      this.showToast('请您输入名称');
+    } else if (this.data.inputName === "太阳花") {
+      wx.navigateTo({
+        url: "../flowersEfficacy/flowersEfficacy"
+      })
+    } else {
+      this.showToast('抱歉 查无此花');
+    }
+  },
+
+  changeData: function (e) {
     this.setData({
       inputName: e.detail.value
-    })
-  },
-  onSearch: function (e) {
-    if (e.keyCode == 13) {
-      if (this.data.inputName === '') {
-        this.showToast('请您输入名称');
-      } else if (this.data.inputName === "太阳花") {
-        wx.navigateTo({
-          url: "../flowersDetail/flowersDetail"
-        })
-      } else {
-        this.showToast('抱歉 查无此花');
-      }
-    }
+    });
   },
 
   showToast(title){
@@ -30,6 +30,6 @@ Page({
 
     setTimeout(function () {
       wx.hideToast()
-    }, 1000)
+    }, 800)
   }
 });
